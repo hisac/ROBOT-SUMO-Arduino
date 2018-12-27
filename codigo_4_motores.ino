c0d20c29699807970fc1e0a18198545712eca31b
Bioelectronica Se escribe con B <35161121+BioelectronicaSeEscribeConB@users.noreply.github.com>
2018-12-27 14:19:55 -0600

    CAPITULO 1

---
+++ codigo_4_motores.ino
@@ -0,0 +1,52 @@
+/* Ejercicio I: Realizar código de control de 4 motores de forma 
+  síncrona hacia adelante, detenerse y atrás. */
+  
+#include <AFMotor.h> //incluimos libreria de shield motor
+AF_DCMotor M1(1); //motor uno conectar en M1
+AF_DCMotor M2(2); //motor uno conectar en M2
+AF_DCMotor M3(3); //motor uno conectar en M3
+AF_DCMotor M4(4); //motor uno conectar en M4
+
+int vel=200; //Variable de velocidad
+
+void setup() 
+{
+  M1.run(RELEASE); //motor inicia apagado
+  M2.run(RELEASE); //motor inicia apagado
+  M3.run(RELEASE); //motor inicia apagado
+  M4.run(RELEASE); //motor inicia apagado
+  delay(2000);
+}
+
+void loop() 
+{   
+  //ADELANTE//
+    M1.setSpeed(vel); //Escribe la velocidad
+    M2.setSpeed(vel); //Escribe la velocidad
+    M3.setSpeed(vel); //Escribe la velocidad
+    M4.setSpeed(vel); //Escribe la velocidad
+    M1.run(FORWARD);  //Gira hacia adelante
+    M2.run(FORWARD);  //Gira hacia adelante
+    M3.run(FORWARD);  //Gira hacia adelante
+    M4.run(FORWARD);  //Gira hacia adelante
+    delay(3000);
+    
+    //DETENERSE//
+    M1.run(RELEASE);  //Gira hacia adelante
+    M2.run(RELEASE);  //Gira hacia adelante
+    M3.run(RELEASE);  //Gira hacia adelante
+    M4.run(RELEASE);  //Gira hacia adelante
+    delay(500);
+   
+   //ATRAS//
+    M1.setSpeed(vel); //Escribe la velocidad
+    M2.setSpeed(vel); //Escribe la velocidad
+    M3.setSpeed(vel); //Escribe la velocidad
+    M4.setSpeed(vel); //Escribe la velocidad
+    M1.run(BACKWARD);  //Gira hacia adelante
+    M2.run(BACKWARD);  //Gira hacia adelante
+    M3.run(BACKWARD);  //Gira hacia adelante
+    M4.run(BACKWARD);  //Gira hacia adelante
+    delay(500);
+
+}
